9f631b61be1ca95571480a1a5f951c6cc83c723b
Jimmy Samalenge <jimmysamalenge@gmail.com>
2018-04-11 12:04:53 +0200

    Update connection.inc.php

--- classes/connection.inc.php
+++ classes/connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost";
-$username = "samaleng_lms_use";
-$password = "+C{UH4N&u9Cs";
-$database = "samaleng_lms_db";
+$host = "";
+$username = "";
+$password = "";
+$database = "";
 
 ?>
